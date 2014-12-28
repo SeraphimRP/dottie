df5ffd2cb1ba6fbcaefcc0aab79f35e31b3aaa16
Elliott Pardee <me@elliottpardee.me>
2014-12-27 22:35:56 -0500

    swm config

---
+++ swmconfig.h
@@ -0,0 +1,16 @@
+#define MOD		SUPER		/* modifier for mouse resize and move */
+
+/* INNER + OUTER should not be greater than BORDERWIDTH */
+#define BORDERWIDTH	3		/*  full  border width */
+#define INNER		2		/*  inner border width */
+#define OUTER		1		/*  outer border width */
+
+/* default colours, don't worry, you can set these with swc later */
+#define FOCUSCOL	0xdc3daf
+#define UNFOCUSCOL	0x3dafdc
+#define OUTERCOL	0x5c5c5c
+
+/* resize and move by mouse? */
+#define ENABLE_MOUSE 0
+/* and double borders? */
+#define DOUBLE_BORDER 1
