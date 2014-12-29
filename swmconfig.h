a74c9f031d7500ade6852db1332f63d831538927
Elliott Pardee <me@elliottpardee.me>
2014-12-29 17:19:53 -0500

    swmconfig.h

--- swmconfig.h
+++ swmconfig.h
@@ -6,7 +6,7 @@
 #define OUTER		1		/*  outer border width */
 
 /* default colours, don't worry, you can set these with swc later */
-#define FOCUSCOL	0xdc3daf
+#define FOCUSCOL	0x3ddc6a
 #define UNFOCUSCOL	0x3dafdc
 #define OUTERCOL	0x5c5c5c
 
